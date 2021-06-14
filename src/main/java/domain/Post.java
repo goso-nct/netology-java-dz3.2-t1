f5fad67da5d32d95ecc2126aa7f3ebdaf40a4e13
Golubev Vladimir <goso@list.ru>
2021-06-14 23:05:43 +0300

    'first'

---
+++ src/main/java/domain/Post.java
@@ -0,0 +1,17 @@
+package domain;
+
+import javax.xml.crypto.Data;
+import java.util.Date;
+
+public class Post {
+    private int id;
+    private String headerText;
+    private Date headerDate;
+    private int headerPictureId;
+    private String text;
+    private int mediaId;
+    private int hearts;
+    private int arrows;
+    private int eyes;
+    private int ownerId;
+}
